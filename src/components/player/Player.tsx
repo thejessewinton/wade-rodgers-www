a85ccaed799a79193ab8ba5ddcfb30925637aa30
jwinton@veroskills.com <jrandallwinton@gmail.com>
2023-03-24 19:08:07 -0400

    add video animation

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,7 +1,7 @@
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { CloseIcon } from "../icons/Icons";
 import VimeoPlayer from "@vimeo/player";
-import { useEffect, useRef } from "react";
+import { Fragment, useEffect, useRef } from "react";
 
 interface PlayerProps {
   open: boolean;
@@ -26,22 +26,34 @@ export const Player = ({ open, onClose }: PlayerProps) => {
   }, [ref, open, onClose]);
 
   return (
-    <Dialog open={open} onClose={onClose}>
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
-        <button
-          onClick={onClose}
-          className="absolute top-4 left-4 transition-transform duration-700 hover:rotate-180"
+    <Transition show={open} as={Fragment}>
+      <Dialog onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
         >
-          <CloseIcon />
-        </button>
-        <iframe
-          ref={ref}
-          src={`https://player.vimeo.com/video/778231216?title=0&byline=0&portrait=0&autoplay=1&branding=0&portait=0&byline=0&title=0`}
-          allowFullScreen
-          loading="lazy"
-          className="min-h-full w-auto min-w-full max-w-none transition-opacity duration-700"
-        />
-      </div>
-    </Dialog>
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
+            <button
+              onClick={onClose}
+              className="absolute top-4 left-4 transition-transform duration-700 hover:rotate-180"
+            >
+              <CloseIcon />
+            </button>
+            <iframe
+              ref={ref}
+              src={`https://player.vimeo.com/video/778231216?title=0&byline=0&portrait=0&autoplay=1&branding=0&portait=0&byline=0&title=0`}
+              allowFullScreen
+              loading="lazy"
+              className="min-h-full w-auto min-w-full max-w-none transition-opacity duration-700"
+            />
+          </div>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
   );
 };
